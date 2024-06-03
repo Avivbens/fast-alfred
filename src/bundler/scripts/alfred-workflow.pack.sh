83532412dc36d0625234fca071f2a00d9b314799
Aviv Ben Shahar <avivbens87@gmail.com>
2024-06-03 16:36:35 +0300

    fix: `pack` script get `targetDir` from config file, if exists 🏗️

--- src/bundler/scripts/alfred-workflow.pack.sh
+++ src/bundler/scripts/alfred-workflow.pack.sh
@@ -3,5 +3,12 @@ set -e
 
 WORKFLOW_NAME=$(jq -r '.name' package.json)
 
+# Extract the targetDir from the .fast-alfred.config.cjs file, if it exists
+CONFIG_FILE="$PWD/.fast-alfred.config.cjs"
+WORKFLOW_BUILD_DIR=$(node -e "try { console.log(require('$CONFIG_FILE')?.bundlerOptions?.targetDir || '') } catch (error) { console.log('') }")
+if [ -z "$WORKFLOW_BUILD_DIR" ]; then
+    WORKFLOW_BUILD_DIR="esbuild"
+fi
+
 # pack to Alfred Workflow
-zip -9 -r "esbuild/$WORKFLOW_NAME.alfredworkflow" *.png *.plist README.md esbuild/** package.json
+zip -9 -r "$WORKFLOW_BUILD_DIR/$WORKFLOW_NAME.alfredworkflow" *.png *.plist README.md esbuild/** package.json
